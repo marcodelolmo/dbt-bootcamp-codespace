8a9ab5a47dc359d3ce9fb2e12f3f3533526f7c81
Marco Antonio Del Olmo <57046310+marcodelolmo@users.noreply.github.com>
2024-02-18 23:11:09 +0000

    src_listings

---
+++ course/dbtlearn/models/src/src_listings.sql
@@ -0,0 +1,13 @@
+WITH raw_listings AS (SELECT * FROM AIRBNB.RAW.RAW_LISTINGS)
+SELECT
+    ID,
+    LISTING_URL,
+    NAME,
+    ROOM_TYPE,
+    MINIMUM_NIGHTS,
+    HOST_ID,
+    PRICE,
+    CREATED_AT,
+    UPDATED_AT
+    FROM
+        raw_listings
